a81b4746557b7ed991469cb30c8dc602f5722cf2
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-04-10 15:46:19 +0100

    Update data.tf

--- modules/kms-key/data.tf
+++ modules/kms-key/data.tf
@@ -8,9 +8,7 @@ data "aws_iam_policy_document" "kms_policy" {
     }
     actions = ["kms:*"]
     resources = ["*"]
-    principal = {
-     service = s3.amazonaws.com
-    }
+    
       #aws_kms_key.my_kms_key.arn,
     #]
   }
