1839ab96471164fc0ea4e1f1369f310d0204dce9
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-04-10 14:54:23 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
+
+variable "alias_name" {
+  description = "The alias to associate with the KMS key"
+  type        = string
+}
+
 module "kms_key_alias" {
   source          = "./modules/kms-key"
   key_description = "My KMS key"
-  alias_name      = "My KMS key alias"
+  alias_name      = "testing"
 }
