c78bd79269b76718dacf2261de6c529de0b8a860
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-04-10 14:44:54 +0100

    Update main.tf

--- modules/kms-key/main.tf
+++ modules/kms-key/main.tf
@@ -1,13 +1,3 @@
-resource "aws_kms_key" "my_kms_key" {
-  description = var.key_description
-  policy      = data.aws_iam_policy_document.kms_policy.json
-}
-
-resource "aws_kms_alias" "my_kms_alias" {
-  name          = "alias/${var.alias_name}"
-  target_key_id = aws_kms_key.my_kms_key.id
-}
-
 data "aws_iam_policy_document" "kms_policy" {
   statement {
     effect  = "Allow"
@@ -16,4 +6,15 @@ data "aws_iam_policy_document" "kms_policy" {
       aws_kms_key.my_kms_key.arn,
     ]
   }
+}
+
+
+resource "aws_kms_key" "my_kms_key" {
+  description = var.key_description
+  policy      = data.aws_iam_policy_document.kms_policy.json
+}
+
+resource "aws_kms_alias" "my_kms_alias" {
+  name          = "alias/${var.alias_name}"
+  target_key_id = aws_kms_key.my_kms_key.id
 }
