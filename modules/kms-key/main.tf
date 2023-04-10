058e9aaeb2c9fcf4a53ec825c98fb64d13d207ec
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-04-10 14:46:06 +0100

    Update main.tf

--- modules/kms-key/main.tf
+++ modules/kms-key/main.tf
@@ -1,13 +1,3 @@
-data "aws_iam_policy_document" "kms_policy" {
-  statement {
-    effect  = "Allow"
-    actions = ["kms:*"]
-    resources = [
-      aws_kms_key.my_kms_key.arn,
-    ]
-  }
-}
-
 
 resource "aws_kms_key" "my_kms_key" {
   description = var.key_description
